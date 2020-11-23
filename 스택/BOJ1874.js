eb9a055b80c8e690ff6282dd825c55e17c7f1646
rktguswjd <010_cm@naver.com>
2020-11-23 22:20:19 +0900

    BOJ1874

--- 스택/BOJ1874.js
+++ 스택/BOJ1874.js
@@ -4,47 +4,85 @@ const rl = readline.createInterface({
   output: process.stdout,
 });
 
-const solution = (input) => {
+// const solution = (input) => {
+//   const n = input.shift();
+//   const stack = [];
+//   let result = "";
+//   let start = 1;
+
+//   for (let i = 0; i < n; i++) {
+//     if (start > n) {
+//       for (let j = i; j < n; j++) {
+//         if (input[j] == stack[stack.length - 1]) {
+//           stack.pop();
+//           result += "-\n";
+//           continue;
+//         }
+//         result = "NO";
+//         break;
+//       }
+//       break;
+//     }
+//     if (start <= input[i]) {
+//       for (let j = start; j <= input[i]; j++) {
+//         stack.push(j);
+//         result += "+\n";
+//         if (j == input[i]) {
+//           stack.pop();
+//           result += "-\n";
+//         }
+//       }
+//       start = input[i] + 1;
+//       continue;
+//     }
+
+//     if (start > input[i]) {
+//       stack.pop();
+//       result += "-\n";
+//     }
+//   }
+
+//   console.log(result);
+// };
+const solution1 = (input) => {
   const n = input.shift();
   const stack = [];
   let result = "";
   let start = 1;
 
-  for (let i = 0; i < n; i++) {
-    if (start > n) {
-      for (let j = i; j < n; j++) {
-        if (input[j] == stack[stack.length - 1]) {
-          stack.pop();
-          result += "-\n";
-          continue;
-        }
+  let pos = 0;
+  let flag = true;
+  while (start <= n) {
+    if (!flag) break;
+    let temp = input[pos];
+    if (start <= temp) {
+      for (let i = start; i <= temp; i++) {
+        stack.push(i);
+        result += "+\n";
+      }
+      start = stack.pop() + 1;
+      result += "-\n";
+    } else {
+      let temp2 = stack.pop();
+      if (temp2 != temp) {
+        flag = false;
+      } else result += "-\n";
+    }
+    pos++;
+  }
+  if (flag) {
+    for (let i = pos; i < n; i++) {
+      if (stack.pop() == input[i]) {
+        result += "-\n";
+        continue;
+      } else {
         result = "NO";
         break;
       }
-      break;
     }
-    if (start <= input[i]) {
-      for (let j = start; j <= input[i]; j++) {
-        stack.push(j);
-        result += "+\n";
-        if (j == input[i]) {
-          stack.pop();
-          result += "-\n";
-        }
-      }
-      start = input[i] + 1;
-      continue;
-    }
-
-    if (start > input[i]) {
-      stack.pop();
-      result += "-\n";
-    }
-  }
-
+  } else result = "NO";
   console.log(result);
 };
-
 const input = [];
 const main = (rl) => {
   rl.on("line", (line) => {
