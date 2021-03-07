aa6909a2d5cbf9fea789b8d4d81517439a337a7c
rktguswjd <010_cm@naver.com>
2021-03-07 17:49:05 +0900

    BOJ15656

---
+++ 백트래킹/BOJ15656.js
@@ -0,0 +1,50 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let n = 0;
+let m = 0;
+const solution = (input) => {
+  const arr = input
+    .shift()
+    .split(" ")
+    .map((n) => +n);
+  n = arr[0];
+  m = arr[1];
+  const numArr = input
+    .shift()
+    .split(" ")
+    .map((n) => +n);
+
+  numArr.sort((a, b) => a - b);
+
+  backtracking([], numArr);
+  console.log(ans.join("\n"));
+};
+
+const ans = [];
+const backtracking = (pickedArr, numArr) => {
+  if (pickedArr.length === m) {
+    ans.push(pickedArr.join(" "));
+    return;
+  }
+
+  for (let i = 0; i < n; i++) {
+    pickedArr.push(numArr[i]);
+    backtracking(pickedArr, numArr);
+    pickedArr.pop();
+  }
+};
+
+const input = [];
+const main = (rl) => {
+  rl.on("line", (line) => {
+    input.push(line);
+  }).on("close", () => {
+    solution(input);
+    process.exit();
+  });
+};
+
+main(rl);
