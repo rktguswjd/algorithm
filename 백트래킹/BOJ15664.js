4d07e0410b4ac86af4f82c8bd1252ce292c6d97d
rktguswjd <010_cm@naver.com>
2021-03-11 16:39:14 +0900

    BOJ15664

---
+++ 백트래킹/BOJ15664.js
@@ -0,0 +1,51 @@
+const readline = require("readline");
+const rl = readline.createInterface({
+  input: process.stdin,
+  output: process.stdout,
+});
+let n = 0;
+let m = 0;
+const solution = (input) => {
+  const arr = input.shift().split(" ");
+  n = +arr[0];
+  m = +arr[1];
+
+  const numArr = input
+    .shift()
+    .split(" ")
+    .map((n) => +n);
+  numArr.sort((a, b) => a - b);
+  const visitedArr = new Array(n).fill(0);
+  backtracking([], numArr, visitedArr, 0);
+  const ans = [...set];
+  console.log(ans.join("\n"));
+};
+
+const set = new Set();
+const backtracking = (pickedArr, numArr, visitedArr, start) => {
+  if (pickedArr.length === m) {
+    set.add(pickedArr.join(" "));
+    return;
+  }
+
+  for (let i = start; i < n; i++) {
+    if (visitedArr[i] === true) continue;
+    visitedArr[i] = true;
+    pickedArr.push(numArr[i]);
+    backtracking(pickedArr, numArr, visitedArr, i);
+    pickedArr.pop();
+    visitedArr[i] = false;
+  }
+};
+
+const input = [];
+const main = (rl) => {
+  rl.on("line", (line) => {
+    input.push(line);
+  }).on("close", () => {
+    solution(input);
+    process.exit();
+  });
+};
+
+main(rl);
